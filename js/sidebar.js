70e5d54f8f00db247ef6774582f047641a879392
Mahmoud Fouda <116393435+mahmoudfoudaa4a@users.noreply.github.com>
2026-07-14 22:12:08 +0300

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,38 +3,36 @@ let aside = document.querySelector("aside");
 let pageOptions = document.querySelectorAll(".pages-options .option");
 
 function initSidebar() {
-  if (window.innerWidth <= 992) {
-    showBtn.addEventListener("click", (e) => {
-      if (window.innerWidth > 992) return;
-      e.stopPropagation();
-      aside.classList.add("show-sidebar");
-      aside.classList.remove("hide-sidebar");
-    });
+    if (window.innerWidth <= 992) {
+        showBtn.addEventListener("click", e => {
+            if (window.innerWidth > 992) return;
+            e.stopPropagation();
+            aside.classList.add("show-sidebar");
+            aside.classList.remove("hide-sidebar");
+        });
 
-    aside.addEventListener("click", (e) => {
-      if (window.innerWidth > 992) return;
-      e.stopPropagation();
-    });
+        aside.addEventListener("click", e => {
+            if (window.innerWidth > 992) return;
+            e.stopPropagation();
+        });
 
-    document.addEventListener("click", () => {
-      if (window.innerWidth > 992) return;
-     if (aside.classList.contains("show-sidebar"))
+        document.addEventListener("click", () => {
+            if (window.innerWidth > 992) return;
+            iaside.classList.remove("show-sidebar");
+            aside.classList.add("hide-sidebar");
+        });
 
-{ aside.classList.remove("show-sidebar");
-      aside.classList.add("hide-sidebar");
-    });}
-
-    pageOptions.forEach((option) => {
-      option.addEventListener("click", () => {
-        if (window.innerWidth > 992) return;
+        pageOptions.forEach(option => {
+            option.addEventListener("click", () => {
+                if (window.innerWidth > 992) return;
+                aside.classList.remove("show-sidebar");
+                aside.classList.add("hide-sidebar");
+            });
+        });
+    } else {
         aside.classList.remove("show-sidebar");
-        aside.classList.add("hide-sidebar");
-      });
-    });
-  } else {
-    aside.classList.remove("show-sidebar");
-    aside.classList.remove("hide-sidebar");
-  }
+        aside.classList.remove("hide-sidebar");
+    }
 }
 initSidebar();
 window.addEventListener("resize", initSidebar);
